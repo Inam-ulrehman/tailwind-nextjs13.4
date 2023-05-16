7f83cab33cb240ff58ae3a9f8d1d77aa226ab6ca
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-16 15:15:00 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    return config
+  },
+}
 
 module.exports = nextConfig
