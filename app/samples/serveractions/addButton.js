8b9f0fb320a06df7384dac1524f1d591b2f5ca4e
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-17 14:58:33 -0400

    fix

--- app/samples/serveractions/addButton.js
+++ app/samples/serveractions/addButton.js
@@ -1,8 +1,9 @@
 'use client'
-import { experimental_useFormStatus as useFormStatus } from 'react-dom'
+// import { experimental_useFormStatus as useFormStatus } from 'react-dom'
 
 export const AddButton = () => {
-  const { pending } = useFormStatus()
+  // const { pending } = useFormStatus()
+  const pending = false
   return (
     <button className='btn-primary w-40' type='submit' disabled={pending}>
       {pending ? 'submitting...' : 'submit'}
