33503297bff9533c72438163ec5400450639b3ce
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-18 13:10:21 -0400

    fix

--- app/components/header/theme.js
+++ app/components/header/theme.js
@@ -4,15 +4,19 @@ import {
   removeItemFromLocalStorage,
   setItemInLocalStorage,
 } from '@/lib/localStorage/localStorage'
+import { VscColorMode } from 'react-icons/vsc'
 import React, { useEffect, useState } from 'react'
-
+const theme = getItemFromLocalStorage('theme')
 const Theme = () => {
+  const [state, setState] = useState('')
   const changeTheme = () => {
     const theme = getItemFromLocalStorage('theme')
     if (!theme) {
+      setState('dark')
       setItemInLocalStorage('theme', 'dark')
       document.documentElement.classList.add('dark')
     } else {
+      setState('light')
       removeItemFromLocalStorage('theme', 'dark')
       document.documentElement.classList.remove('dark')
     }
@@ -20,12 +24,19 @@ const Theme = () => {
   useEffect(() => {
     const theme = getItemFromLocalStorage('theme')
     if (theme) {
+      setState('dark')
       document.documentElement.classList.add('dark')
     }
   }, [])
   return (
     <div>
-      <button onClick={changeTheme}>Change theme</button>
+      <button
+        className='flex items-center gap-1 border-2 px-2 rounded-md  hover:bg-slate-100 dark:hover:bg-slate-700'
+        onClick={changeTheme}
+      >
+        {state === 'dark' ? 'Light' : 'Dark'} theme
+        <VscColorMode />
+      </button>
     </div>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -17,9 +17,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='bg-white dark:bg-black'>
-          <h1 className='dark:text-white'>hello</h1>
-        </div>
         <Header />
         <main>
           <Providers>{children}</Providers>
