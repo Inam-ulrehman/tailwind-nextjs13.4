3c5aeff9f1f5bc5b9a4c59d1ff73150eec8db755
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-18 15:21:58 -0400

    fix

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import CopyRight from './copyRight'
+import NavLinks from './navLinks'
+import SocialMedia from './socialMedia'
+import Address from './address'
+import Logo from './logo'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='grid border-2   '>
+      <Logo />
+      <div className='grid m-auto lg:grid-cols-3 py-5 '>
+        <NavLinks />
+        <SocialMedia />
+        <Address />
+      </div>
       <CopyRight></CopyRight>
     </footer>
   )
