c8ce309e959e8a3fbe79df0bf61fb54483830127
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-05-18 14:08:08 -0400

    fix

--- app/components/footer/copyRight.js
+++ app/components/footer/copyRight.js
@@ -3,7 +3,7 @@ import { FaRegCopyright } from 'react-icons/fa'
 
 const CopyRight = () => {
   return (
-    <div className='flex items-center p-5 justify-center text-sm text-gray-500'>
+    <div className='flex flex-wrap border-2 items-center p-5 justify-center text-sm text-gray-500'>
       Copyright
       <span className='px-2'>
         <FaRegCopyright />
